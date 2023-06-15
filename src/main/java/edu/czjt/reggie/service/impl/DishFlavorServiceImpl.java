f50cc761840cfd35b9adf596d9a238147a545ceb
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 18:50:34 +0800

    Update DishFlavorServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/DishFlavorServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/DishFlavorServiceImpl.java
@@ -9,9 +9,16 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ *@Author zhang_bingru20855066
+ *@Date 2023/6/15 17:57
+*/
 
 @Service
 public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+    //实现了DishFlavorService接口，继承了ServiceImpl类
+    //DishFlavor表示菜品口味，DishFlavorService是对菜品口味进行操作的服务接口
+    //ServiceImpl可以简化Service层的代码编写。
     @Override
     public List<DishFlavor> getFlavorsByDishId(Long dishId) {
         LambdaQueryWrapper<DishFlavor> dishFlavorLambdaQueryWrapper = new LambdaQueryWrapper<>();
@@ -25,4 +32,5 @@ public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFla
         dishFlavorLambdaQueryWrapper.eq(DishFlavor::getDishId, dishId);
         return this.remove(dishFlavorLambdaQueryWrapper);
     }
+    
 }
