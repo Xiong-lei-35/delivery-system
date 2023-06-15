4afd47168791ce40da60e5768e22c2903d6910b4
Xiong-lei-35 <135949936+Xiong-lei-35@users.noreply.github.com>
2023-06-15 09:05:24 +0800

    Update DishFlavorServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/DishFlavorServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/DishFlavorServiceImpl.java
@@ -9,9 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-/**
- * Created by jinkun.tian on 2023/4/27
- */
+
 @Service
 public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
     @Override
