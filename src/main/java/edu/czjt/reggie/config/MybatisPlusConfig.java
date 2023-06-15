52e69474bb63a2be31e559f321c9d2d3dec6a0d7
poiuqwerr <130961274+poiuqwerr@users.noreply.github.com>
2023-06-15 08:52:48 +0800

    Update MybatisPlusConfig.java

--- src/main/java/edu/czjt/reggie/config/MybatisPlusConfig.java
+++ src/main/java/edu/czjt/reggie/config/MybatisPlusConfig.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * 配置MP的分页插件
- * Created by jinkun.tian on 2023/3/21
- */
+
 @Configuration
 public class MybatisPlusConfig {
 
