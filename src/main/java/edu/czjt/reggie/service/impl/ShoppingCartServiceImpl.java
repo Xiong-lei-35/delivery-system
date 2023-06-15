02c1c712f55e8474c13f3aa0c4847698152410d0
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 18:53:22 +0800

    Update ShoppingCartServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/ShoppingCartServiceImpl.java
@@ -6,6 +6,11 @@ import edu.czjt.reggie.mapper.ShoppingCartMapper;
 import edu.czjt.reggie.service.ShoppingCartService;
 import org.springframework.stereotype.Service;
 
+/**
+ *@Author zhang_bingru20855066
+ *@Date 2023/6/15 17:57
+*/
+
 @Service
 public class ShoppingCartServiceImpl extends ServiceImpl<ShoppingCartMapper, ShoppingCart> implements ShoppingCartService {
 
