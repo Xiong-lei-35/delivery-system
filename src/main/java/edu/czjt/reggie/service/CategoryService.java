666cb11d468be3d5d298b31a88899724e30ce43f
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 18:40:30 +0800

    Update CategoryService.java
    添加个人注释

--- src/main/java/edu/czjt/reggie/service/CategoryService.java
+++ src/main/java/edu/czjt/reggie/service/CategoryService.java
@@ -2,5 +2,12 @@ package edu.czjt.reggie.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.Category;
+
+/**
+ *@Author zhang_bingru20855066
+ *@Date 2023/6/15 17:57
+*/
+
 public interface CategoryService extends IService<Category> {
+  
 }
