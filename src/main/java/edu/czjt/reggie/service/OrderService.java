9bbdd1d935ebf529d3b23bef7f06b5279f7d6bf3
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 18:43:11 +0800

    Update OrderService.java

--- src/main/java/edu/czjt/reggie/service/OrderService.java
+++ src/main/java/edu/czjt/reggie/service/OrderService.java
@@ -3,13 +3,22 @@ package edu.czjt.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.Orders;
 
+/**
+ *@Author zhang_bingru20855066
+ *@Date 2023/6/15 17:57
+*/
+
 public interface OrderService extends IService<Orders> {
 
     /**
      * 用户下单
      * @param orders
      */
-    public void submit(Orders orders);
 
+    //submit方法可能是将订单提交到某个系统或者服务中进行处理
+    public void submit(Orders orders);
+    
+    //，而submitAgain方法可能是重新提交之前失败的订单
     public void submitAgain(Orders orders);
+    
 }
