564c86d691f16f318a48bfe0f673d1c8eb154f2e
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 18:33:05 +0800

    Update AddressBookService.java

--- src/main/java/edu/czjt/reggie/service/AddressBookService.java
+++ src/main/java/edu/czjt/reggie/service/AddressBookService.java
@@ -3,6 +3,11 @@ package edu.czjt.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.entity.AddressBook;
 
+/**
+ *@Author zhang_bingru20855066
+ *@Date 2023/6/15 17:57
+*/
+
 public interface AddressBookService extends IService<AddressBook> {
 
 }
