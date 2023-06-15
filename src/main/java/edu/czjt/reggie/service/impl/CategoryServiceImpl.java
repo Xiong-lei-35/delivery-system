ac6cd62ac7f4cc4cd411de0cdae5c66d9d9082b3
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 08:54:53 +0800

    Update CategoryServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/CategoryServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/CategoryServiceImpl.java
@@ -6,9 +6,6 @@ import edu.czjt.reggie.mapper.CategoryMapper;
 import edu.czjt.reggie.service.CategoryService;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by jinkun.tian on 2023/4/27
- */
 @Service
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 }
