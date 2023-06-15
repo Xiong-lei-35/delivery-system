1d3a5aa14f2fcc1253e63fbc4bed6fc45ba15bf0
Xiong-lei-35 <135949936+Xiong-lei-35@users.noreply.github.com>
2023-06-15 09:02:58 +0800

    Update DishService.java

--- src/main/java/edu/czjt/reggie/service/DishService.java
+++ src/main/java/edu/czjt/reggie/service/DishService.java
@@ -5,7 +5,6 @@ import edu.czjt.reggie.dto.DishDto;
 import edu.czjt.reggie.entity.Dish;
 
 /**
- * Created by jinkun.tian on 2023/4/27
  */
 public interface DishService extends IService<Dish> {
     public DishDto getByIdWithFlavor(Long id);
