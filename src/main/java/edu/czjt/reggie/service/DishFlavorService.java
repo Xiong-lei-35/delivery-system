fa61089d10fbf526b97b2df7f5e97355a94e8c26
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 08:54:01 +0800

    Update DishFlavorService.java

--- src/main/java/edu/czjt/reggie/service/DishFlavorService.java
+++ src/main/java/edu/czjt/reggie/service/DishFlavorService.java
@@ -5,9 +5,7 @@ import edu.czjt.reggie.entity.DishFlavor;
 
 import java.util.List;
 
-/**
- * Created by jinkun.tian on 2023/4/27
- */
+
 public interface DishFlavorService extends IService<DishFlavor> {
 
     public List<DishFlavor> getFlavorsByDishId(Long dishId);
