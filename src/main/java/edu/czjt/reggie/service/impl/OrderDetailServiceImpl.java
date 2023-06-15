b0a0be254b81e78801e316faf3c5a79a06aa8803
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 18:51:48 +0800

    Update OrderDetailServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/OrderDetailServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/OrderDetailServiceImpl.java
@@ -7,6 +7,11 @@ import edu.czjt.reggie.mapper.OrderDetailMapper;
 import edu.czjt.reggie.service.OrderDetailService;
 import org.springframework.stereotype.Service;
 
+/**
+ *@Author zhang_bingru20855066
+ *@Date 2023/6/15 17:57
+*/
+
 @Service
 public class OrderDetailServiceImpl extends ServiceImpl<OrderDetailMapper, OrderDetail> implements OrderDetailService {
 
