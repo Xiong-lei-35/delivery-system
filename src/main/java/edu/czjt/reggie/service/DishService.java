29c9654c0a6ef118b92512a6880c3a7564c8a66b
Xiong-lei-35 <135949936+Xiong-lei-35@users.noreply.github.com>
2023-06-15 09:04:11 +0800

    Update DishService.java

--- src/main/java/edu/czjt/reggie/service/DishService.java
+++ src/main/java/edu/czjt/reggie/service/DishService.java
@@ -4,8 +4,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.dto.DishDto;
 import edu.czjt.reggie.entity.Dish;
 
-/**
- */
 public interface DishService extends IService<Dish> {
     public DishDto getByIdWithFlavor(Long id);
 
