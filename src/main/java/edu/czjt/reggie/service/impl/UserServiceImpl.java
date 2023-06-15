931b99f77f27e8290229dd07df8e003c0cc788fd
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 08:56:17 +0800

    Update UserServiceImpl.java

--- src/main/java/edu/czjt/reggie/service/impl/UserServiceImpl.java
+++ src/main/java/edu/czjt/reggie/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import edu.czjt.reggie.mapper.UserMapper;
 import edu.czjt.reggie.service.UserService;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 }
