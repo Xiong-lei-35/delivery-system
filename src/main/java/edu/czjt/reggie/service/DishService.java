32f6aca2bb3d3a0f6c03603b51beb22410ed5c70
zhangbingru20855066 <135928351+zhangbingru20855066@users.noreply.github.com>
2023-06-15 18:41:20 +0800

    Update DishService.java

--- src/main/java/edu/czjt/reggie/service/DishService.java
+++ src/main/java/edu/czjt/reggie/service/DishService.java
@@ -4,8 +4,15 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import edu.czjt.reggie.dto.DishDto;
 import edu.czjt.reggie.entity.Dish;
 
+/**
+ *@Author zhang_bingru20855066
+ *@Date 2023/6/15 17:57
+*/
+
 public interface DishService extends IService<Dish> {
+    
     public DishDto getByIdWithFlavor(Long id);
 
     public void saveWithFlavor(DishDto dishDto);
+    
 }
